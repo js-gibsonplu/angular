cb0ec4117bfd74e7b4dd7e62ae61862d098528ed
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-09-18 06:38:32 -0500

    sdgfsdgfsd

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverID = 1546;
   status = 'online';
+  newInstance = true;
+  moreText = 'Show even more!';
+
+  constructor() {
+    setTimeout(()=> {
+      this.newInstance = false;
+    }, 3000);
+  }
+
+  onMoreTextClick() {
+    console.log('more text button clicked')
+    this.moreText += ' YES MOAR!!!!!!!!!!!!'
+  }
 }
