f62e965c342f368c04f98e35491fae04bb3f5b3d
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-09-20 06:56:30 -0500

    working through the course

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
   username = '';
 
-  constructor() {}
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.username);
+  }
+
+  onResetUN () {
+    this.username = "";
+  }
 }
