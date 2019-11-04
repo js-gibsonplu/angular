5934a2da6fbbade34dae541eb12ec987aa72ede9
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-11-04 06:40:36 -0600

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './components/game-control/game-control.component';
+import { OddComponent } from './components/odd/odd.component';
+import { EvenComponent } from './components/even/even.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GameControlComponent, OddComponent, EvenComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GameControlComponent, OddComponent, EvenComponent]
 })
 export class AppModule {}
