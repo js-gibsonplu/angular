17f199c4774ec864e8fbad10acbc019db4009fb9
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-11-04 06:37:54 -0600

    removed junk to do assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, NavBarComponent, WarningAlertComponent, SuccessAlertComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
