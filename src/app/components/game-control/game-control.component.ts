030e36bcb77083c24d4d610646c91204d5408867
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-11-06 07:47:47 -0600

    working on emitting events

--- src/app/components/game-control/game-control.component.ts
+++ src/app/components/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() myNum = 0;
+  @Output() gameStarted = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onGameStart(data) {
+    const gameTimer = setInterval(() => {
+      this.gameStarted.emit(this.myNum);
+      this.myNum++;
+      console.log(this.myNum);
+    }, 1000);
   }
-
+  // TODO this needs work to stop the interval - this current config doesn't work
+  onGameStop(func) {
+    clearInterval(func.gameTimer);
+  }
 }
